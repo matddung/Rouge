f9c96e5c1409b2543bd10f208ac25a1755bbb9b8
matddung <matddung1111@gmail.com>
2025-05-12 22:08:12 +0900

    before refactoring

--- Source/Rogue/RogueCharacterCombatComponent.h
+++ Source/Rogue/RogueCharacterCombatComponent.h
@@ -6,22 +6,18 @@
 #include "Components/ActorComponent.h"
 #include "RogueCharacterCombatComponent.generated.h"
 
-
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ROGUE_API URogueCharacterCombatComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	URogueCharacterCombatComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 		
